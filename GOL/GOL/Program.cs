2eb030a946326776bd21924980fed7abdf45f9fe
Govnoedick <prodimos@mail.ru>
2024-09-21 19:50:31 +0800

    1.Добавлен нормальный нейминг папок
    2.Добавленно описание ридми файла и ридми.тхт (для тех у кого проблемы с открытием .мд файла)
    
    3.Переименованна папка "fields" в "rules" и добавлен файл с правилами, но сами правила пока что не добавлены :)
    
    3.Теперь путь относительный и должен работать у всех
    
    4.Теперь переменный размер буфера, что должно улучшить оптимизацию

---
+++ GOL/GOL/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace GOL {
+	class Program {
+
+		static void Main(string[] args) {
+			Console.CursorVisible = false;
+
+			//1. если рядом с живой клеткой менее 2-ух живых, то она умирает
+			//2. если рядом с живой клеткой более 3-ех живых, то она умирает
+			//3. если рядом с мертвой клеткой ровно 3 живых, то она рождается
+
+			//точка - мертвая клетка, собака - живая (./@)
+
+
+			string pathToField=System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\rules\\field.txt");
+
+			string[] fld = File.ReadAllLines(pathToField);
+			Console.SetWindowSize(fld[0].Length, fld.Length);
+
+			Console.BufferHeight = fld.Length + 1;
+			Console.BufferWidth = fld[0].Length + 1;
+			
+			List<string> fld2 = new List<string>(fld);
+			foreach(var el in fld2) {
+				Console.WriteLine(el);
+			}
+			Console.SetCursorPosition(0, 0);
+			
+			
+
+			while(true){
+				for(int i = 1; i < fld.Length - 1; i++) {//с еденичек до n - 1 что бы не было лишней итерации на границы и эксепшенов
+					for(int j = 1; j < fld[i].Length - 1; j++){
+						int neighbrs = Check(fld, i, j);
+						if(fld[i][j] == '@'){
+							if(neighbrs > 3 || neighbrs < 2){
+								fld2[i] = fld2[i].Remove(j, 1).Insert(j, ".");
+							}
+						}else if(neighbrs == 3){
+							fld2[i] = fld2[i].Remove(j, 1).Insert(j, "@");
+						}
+					}
+				}
+				fld = fld2.ToArray();
+				foreach(var ln in fld){
+					Console.WriteLine(ln);
+				}
+				
+				Console.SetCursorPosition(0, 0);
+				Thread.Sleep(1000);
+				
+				
+			}
+
+		}
+		private static int Check(string[] field, int row, int column){
+			int counter = 0;
+			if(field[row - 1][column] == '@'){
+				counter++;
+			}
+			if(field[row][column - 1] == '@'){
+				counter++;
+			}
+			if(field[row - 1][column - 1] == '@'){
+				counter++;
+			}
+			if(field[row - 1][column + 1] == '@'){
+				counter++;
+			}
+			if(field[row + 1][column] == '@'){
+				counter++;
+			}
+			if(field[row + 1][column - 1] == '@'){
+				counter++;
+			}
+			if(field[row + 1][column + 1] == '@'){
+				counter++;
+			}
+			if(field[row][column + 1] == '@'){
+				counter++;
+			}
+			return counter;
+		}
+		
+	}
+}
